3339ef65dbe7f3a63712f6af0ae14933e0641de5
Александр <hyperpositron@gmail.com>
2023-06-21 21:54:09 +0200

    first commit

---
+++ src/main/java/pro/sky/hogwards/Howework1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Howework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Howework1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hogwards/Howework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Howework1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
